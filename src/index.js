3df79e7f34307d8ac6183b1ffe324adec8603079
Nikhil Mudgal <nikhilm2097@gmail.com>
2022-07-18 01:51:46 +0530

    Setup socket.io in client side

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express();
 const server = http.createServer(app)
+const io = socketio(server) // now our server supports web sockets
+
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
+
+io.on('connection', () => {
+ console.log('new web socket connection')
+})
 
 server.listen(port, () => {
     console.log('Server is up on ' + port);
