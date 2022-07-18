059fa3241a2d6011f4817c3c47d6d1134290ee74
Nikhil Mudgal <nikhilm2097@gmail.com>
2022-07-19 01:13:38 +0530

    Basics of Sockets.io

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,10 @@
-io()
+const socket = io()
+
+socket.on('countUpdated', (data) => {  // client receive data from the server
+    console.log('The count has been updated ', data);
+})
+
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked')
+    socket.emit('increment')
+})
