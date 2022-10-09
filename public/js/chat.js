a9bb6849c390db0ffb0c83fbc85da5978a2009e7
Nikhil Mudgal <nikhilm2097@gmail.com>
2022-10-09 08:21:16 +0530

    Event Acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,40 @@
 const socket = io() // to connect to the server
 
+const $messagFormButton = document.querySelector('#submit')
+const $messagFormInput = document.querySelector('#messsge')
+
 socket.on('message', (data) => {  // client receive data from the server
     console.log(data);
 })
 
-document.querySelector('#submit').addEventListener('click', (e) => {
+$messagFormButton.addEventListener('click', (e) => {
     e.preventDefault();
+    $messagFormButton.setAttribute('disabled', 'true')
+
     const message = document.querySelector('#messsge').value;
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+        $messagFormButton.removeAttribute('disabled')
+        $messagFormInput.value = ''
+        $messagFormInput.focus()
+        // console.log('The message was ', message)
+        if(error) {
+            return console.log(error)
+        }
+        console.log('Message Delivered')
+    })
 })
+
+document.querySelector('#sendLocation').addEventListener('click', (e) => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supportive by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude
+        const long = position.coords.longitude
+        socket.emit('sendLocation', {lat, long}, () => {
+            console.log('Location Shared');
+        })
+    });
+});
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express();
 const server = http.createServer(app)
@@ -14,11 +15,26 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 
-io.on('connection', (socket) => { // socket is an object that contains info about new connection. Everytime a new connection is established with a server
+io.on('connection', (socket) => { // socket is an object that contains info about new connection. Everytime a new connection is established with a server 'connection' is a built in event
  socket.emit('message', "Welcome") // emiting an event
- socket.on('sendMessage', (data) => { //receiving a message when event triggered
-     io.emit('message', data) // emit a connection to every single connection available
- });
+ socket.broadcast.emit('message', 'A new USer has joined') // to emit event to everyone except the one who has emit an event
+ socket.on('sendMessage', (data, callback) => { //receiving a message when event triggered
+    const filter = new Filter()
+    
+    if(filter.isProfane(data)) {
+        return callback('Profanity is not allowed')
+    }
+    io.emit('message', data) // emit a connection to every single connection available
+    callback('Delivered! to users')
+    });
+ socket.on('sendLocation', (coordinates, callback) => {
+    // io.emit('message', 'Location: ' + coordinates.lat + ',' + coordinates.long);
+    io.emit('message', `https://google.com/maps?q=${coordinates.lat},${coordinates.long}`)
+    callback(); // callback is sent to only that user who has triggered the event
+ })
+ socket.on('disconnect', () => { // disconnecr is a built in event which triggers when a user is disconnected from a server
+    io.emit('message', 'A USer has left')
+ })
 })
 
 server.listen(port, () => {
